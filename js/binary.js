b68ee89a5b5aa44694bfe6dde98d82b6287b0a35
Moeez <159162874+moeez5251@users.noreply.github.com>
2024-05-09 18:08:08 +0500

    Add files via upload

--- js/binary.js
+++ js/binary.js
@@ -20,7 +20,52 @@ menu[0].addEventListener('click', () => {
         menu[2].style.color = 'grey';
         menu[3].style.color = 'grey';
         inp.type = 'number';
-        inp.addEventListener('input', input1);
+        inp.addEventListener('input', () => {
+                let inputvalue = inp.value;
+                let valid = true;
+                for (let i = 0; i < inputvalue.length; i++) {
+                        if (inputvalue[i] !== "0" && inputvalue[i] !== "1") {
+                                valid = false;
+                                break;
+                        }
+                }
+                if (!valid) {
+                        inp.classList.add('red');
+                } else {
+                        inp.classList.remove('red');
+                }
+                let Decimal = document.getElementById('first');
+                let Octal = document.getElementById('second');
+                let Hexa = document.getElementById('third');
+                let Decimalvalue = parseInt(inputvalue, 2);
+                Decimal.value = Decimalvalue;
+                Octal.value = Decimalvalue.toString(8);
+                Hexa.value = Decimalvalue.toString(16).toUpperCase();
+
+                // ==========> Complements Conversion <===========
+
+                let one = document.getElementById('One');
+                let a = inp.value;
+                let result = '';
+                for (let i = 0; i < a.length; i++) {
+                        if (a[i] == 1) {
+                                result += '0';
+                        }
+                        else if (a[i] == 0) {
+                                result += '1';
+
+                        }
+                }
+                one.value = result;
+                let Two = document.getElementById('Two');
+                if (inp != "") {
+
+                        let b = parseInt(result, 2);
+                        b++;
+                        Two.value = b.toString(2);
+
+                }
+        });
         document.getElementById('first').value = "";
         document.getElementById('second').value = "";
         document.getElementById('third').value = "";
@@ -45,7 +90,30 @@ menu[1].addEventListener('click', () => {
         menu[2].style.color = 'grey';
         menu[3].style.color = 'grey';
         inp.type = 'number';
-        inp.addEventListener('input', input2)
+        inp.addEventListener('input', () => {
+                let Decimal = document.getElementById('fourth');
+                let Octal = document.getElementById('fifth');
+                let Hexa = document.getElementById('sixth');
+                let inputvalue = inp.value.trim();
+                let Decimalvalue = parseInt(inputvalue, 8);
+                Decimal.value = Decimalvalue;
+                Octal.value = Decimalvalue.toString(2);
+                Hexa.value = Decimalvalue.toString(16).toUpperCase();
+                let valid = true;
+                for (let i = 0; i < inputvalue.length; i++) {
+                        if (inputvalue[i] !== "0" && inputvalue[i] !== "1" && inputvalue[i] !== "2" && inputvalue[i] !== "3" && inputvalue[i] !== "4" && inputvalue[i] !== "5" && inputvalue[i] !== "6" && inputvalue[i] !== "7") {
+                                valid = false;
+                                break;
+                        }
+                }
+                if (!valid) {
+                        inp.classList.add('red');
+
+                } else {
+                        inp.classList.remove('red');
+
+                }
+        });
         document.getElementById('fourth').value = "";
         document.getElementById('fifth').value = "";
         document.getElementById('sixth').value = "";
@@ -66,7 +134,30 @@ menu[2].addEventListener('click', () => {
         menu[2].style.color = 'white';
         menu[3].style.color = 'grey';
         inp.type = 'text';
-        inp.addEventListener('input', input3)
+        inp.addEventListener('input', () => {
+                let Decimal = document.getElementById('seventh');
+                let Octal = document.getElementById('eighth');
+                let Hexa = document.getElementById('ninth');
+                let inputvalue = inp.value.trim();
+                let Decimalvalue = parseInt(inputvalue, 16);
+                Decimal.value = Decimalvalue;
+                Octal.value = Decimalvalue.toString(2);
+                Hexa.value = Decimalvalue.toString(8);
+                let valid = true;
+                for (let i = 0; i < inputvalue.length; i++) {
+                        if (inputvalue[i] !== "0" && inputvalue[i] !== "1" && inputvalue[i] !== "2" && inputvalue[i] !== "3" && inputvalue[i] !== "4" && inputvalue[i] !== "5" && inputvalue[i] !== "6" && inputvalue[i] !== "7" && inputvalue[i] !== "8" && inputvalue[i] !== "9" && inputvalue[i] !== "A" && inputvalue[i] !== "B" && inputvalue[i] !== "C" && inputvalue[i] !== "D" && inputvalue[i] !== "E" && inputvalue[i] !== "F" && inputvalue[i] !== "a" && inputvalue[i] !== "b" && inputvalue[i] !== "c" && inputvalue[i] !== "d" && inputvalue[i] !== "e" && inputvalue[i] !== "f") {
+                                valid = false;
+                                break;
+                        }
+                }
+                if (!valid) {
+                        inp.classList.add('red');
+
+                } else {
+                        inp.classList.remove('red');
+
+                }
+        });
         document.getElementById('seventh').value = "";
         document.getElementById('eighth').value = "";
         document.getElementById('ninth').value = "";
@@ -91,130 +182,33 @@ menu[3].addEventListener('click', () => {
         document.getElementById('eleventh').value = "";
         document.getElementById('twelveth').value = "";
         inp.value = "";
-        inp.addEventListener('input', input4)
+        inp.addEventListener('input', () => {
+                let Binary = document.getElementById('tenth');
+                let Octal = document.getElementById('eleventh');
+                let Hexa = document.getElementById('twelveth');
+                let inputValue = parseInt(inp.value.trim());
+                Binary.value = inputValue.toString(2);
+                Octal.value = inputValue.toString(8);
+                Hexa.value = inputValue.toString(16).toUpperCase();
+                let valid = true;
+                for (let i = 0; i < inputValue.length; i++) {
+                        if (inputValue[i] !== "0" && inputValue[i] !== "1" && inputValue[i] !== "2" && inputValue[i] !== "3" && inputValue[i] !== "4" && inputValue[i] !== "5" && inputValue[i] !== "6" && inputValue[i] !== "7"&&inputValue!=="9") {
+                                valid = false;
+                                break;
+                        }
+                }
+                if (!valid) {
+                        inp.classList.add('red');
+
+                } else {
+                        inp.classList.remove('red');
+
+                }
+        })
 
 });
 
-// ==========> Functions for Calculations <===========
 
-function input1() {
-        let Decimal = document.getElementById('first');
-        let Octal = document.getElementById('second');
-        let Hexa = document.getElementById('third');
-        let inputvalue = inp.value.trim();
-        let Decimalvalue = parseInt(inputvalue, 2);
-        Decimal.value = Decimalvalue;
-        Octal.value = Decimalvalue.toString(8);
-        Hexa.value = Decimalvalue.toString(16).toUpperCase();
-        let valid = true;
-        for (let i = 0; i < inputvalue.length; i++) {
-                if (inputvalue[i] !== "0" && inputvalue[i] !== "1") {
-                        valid = false;
-                        break;
-                }
-        }
-        if (!valid) {
-                inp.classList.add('red');
-        } else {
-                inp.classList.remove('red');
-        }
-
-        // ==========> Complements Conversion <===========
-
-        let one = document.getElementById('One');
-        let a = inp.value;
-        let result = '';
-        for (let i = 0; i < a.length; i++) {
-                if (a[i] == 1) {
-                        result += '0';
-                }
-                else if (a[i] == 0) {
-                        result += '1';
-
-                }
-        }
-        one.value = result;
-        let Two = document.getElementById('Two');
-        if (inp != "") {
-
-                let b = parseInt(result, 2);
-                b++;
-                Two.value = b.toString(2);
-
-        }
-}
-function input2() {
-        let Decimal = document.getElementById('fourth');
-        let Octal = document.getElementById('fifth');
-        let Hexa = document.getElementById('sixth');
-        let inputvalue = inp.value.trim();
-        let Decimalvalue = parseInt(inputvalue, 8);
-        Decimal.value = Decimalvalue;
-        Octal.value = Decimalvalue.toString(2);
-        Hexa.value = Decimalvalue.toString(16).toUpperCase();
-        let valid = true;
-        for (let i = 0; i < inputvalue.length; i++) {
-                if (inputvalue[i] !== "0" && inputvalue[i] !== "1" && inputvalue[i] !== "2" && inputvalue[i] !== "3" && inputvalue[i] !== "4" && inputvalue[i] !== "5" && inputvalue[i] !== "6" && inputvalue[i] !== "7") {
-                        valid = false;
-                        break;
-                }
-        }
-        if (!valid) {
-                inp.classList.add('red');
-
-        } else {
-                inp.classList.remove('red');
-
-        }
-
-}
-function input3() {
-        let Decimal = document.getElementById('seventh');
-        let Octal = document.getElementById('eighth');
-        let Hexa = document.getElementById('ninth');
-        let inputvalue = inp.value.trim();
-        let Decimalvalue = parseInt(inputvalue, 16);
-        Decimal.value = Decimalvalue;
-        Octal.value = Decimalvalue.toString(2);
-        Hexa.value = Decimalvalue.toString(8);
-        let valid = true;
-        for (let i = 0; i < inputvalue.length; i++) {
-                if (inputvalue[i] !== "0" && inputvalue[i] !== "1" && inputvalue[i] !== "2" && inputvalue[i] !== "3" && inputvalue[i] !== "4" && inputvalue[i] !== "5" && inputvalue[i] !== "6" && inputvalue[i] !== "7" && inputvalue[i] !== "8" && inputvalue[i] !== "9" && inputvalue[i] !== "A" && inputvalue[i] !== "B" && inputvalue[i] !== "C" && inputvalue[i] !== "D" && inputvalue[i] !== "E" && inputvalue[i] !== "F"&& inputvalue[i] !== "a"&& inputvalue[i] !== "b"&& inputvalue[i] !== "c"&& inputvalue[i] !== "d"&& inputvalue[i] !== "e"&& inputvalue[i] !== "f") {
-                        valid = false;
-                        break;
-                }
-        }
-        if (!valid) {
-                inp.classList.add('red');
-
-        } else {
-                inp.classList.remove('red');
-
-        }
-}
-function input4() {
-        let Binary = document.getElementById('tenth');
-        let Octal = document.getElementById('eleventh');
-        let Hexa = document.getElementById('twelveth');
-        let inputValue = parseInt(inp.value.trim());
-        Binary.value = inputValue.toString(2);
-        Octal.value = inputValue.toString(8);
-        Hexa.value = inputValue.toString(16).toUpperCase();
-        let valid = true;
-        for (let i = 0; i < inputValue.length; i++) {
-                if (inputValue[i] !== "0" && inputValue[i] !== "1" && inputValue[i] !== "2" && inputValue[i] !== "3" && inputValue[i] !== "4" && inputValue[i] !== "5" && inputValue[i] !== "6" && inputValue[i] !== "7" && inputValue[i] !== "8" && inputValue[i] !== "9") {
-                        valid = false;
-                        break;
-                }
-        }
-        if (!valid) {
-                inp.classList.add('red');
-
-        } else {
-                inp.classList.remove('red');
-
-        }
-}
 
 // =================>Themes<==============
 let theme = document.querySelector('.theme');
@@ -239,9 +233,9 @@ menu[4].addEventListener('click', () => {
         inputs[0].value = ""
         inputs[1].value = ""
         result.value = ""
-        inputs[0].addEventListener('input',()=>{
+        inputs[0].addEventListener('input', () => {
                 let valid = true;
-                let d=inputs[0].value;
+                let d = inputs[0].value;
                 for (let i = 0; i < d.length; i++) {
                         if (d[i] !== "0" && d[i] !== "1") {
                                 valid = false;
@@ -254,9 +248,9 @@ menu[4].addEventListener('click', () => {
                         inputs[0].classList.remove('red');
                 }
         });
-        inputs[1].addEventListener('input',()=>{
+        inputs[1].addEventListener('input', () => {
                 let valid = true;
-                let d=inputs[1].value;
+                let d = inputs[1].value;
                 for (let i = 0; i < d.length; i++) {
                         if (d[i] !== "0" && d[i] !== "1") {
                                 valid = false;
@@ -312,9 +306,9 @@ menu[5].addEventListener('click', () => {
         menu[4].style.color = 'grey';
         menu[5].style.color = 'white';
         menu[6].style.color = 'grey';
-        inputs[0].addEventListener('input',()=>{
+        inputs[0].addEventListener('input', () => {
                 let valid = true;
-                let d=inputs[0].value;
+                let d = inputs[0].value;
                 for (let i = 0; i < d.length; i++) {
                         if (d[i] !== "0" && d[i] !== "1" && d[i] !== "2" && d[i] !== "3" && d[i] !== "4" && d[i] !== "5" && d[i] !== "6" && d[i] !== "7") {
                                 valid = false;
@@ -327,9 +321,9 @@ menu[5].addEventListener('click', () => {
                         inputs[0].classList.remove('red');
                 }
         });
-        inputs[1].addEventListener('input',()=>{
+        inputs[1].addEventListener('input', () => {
                 let valid = true;
-                let d=inputs[1].value;
+                let d = inputs[1].value;
                 for (let i = 0; i < d.length; i++) {
                         if (d[i] !== "0" && d[i] !== "1" && d[i] !== "2" && d[i] !== "3" && d[i] !== "4" && d[i] !== "5" && d[i] !== "6" && d[i] !== "7") {
                                 valid = false;
@@ -385,11 +379,11 @@ menu[6].addEventListener('click', () => {
         inputs[1].value = ""
         result.value = ""
 
-        inputs[0].addEventListener('input',()=>{
+        inputs[0].addEventListener('input', () => {
                 let valid = true;
-                let d=inputs[0].value;
+                let d = inputs[0].value;
                 for (let i = 0; i < d.length; i++) {
-                        if (d[i] !== "0" && d[i] !== "1" && d[i] !== "2" && d[i] !== "3" && d[i] !== "4" && d[i] !== "5" && d[i] !== "6" && d[i] !== "7" && d[i] !== "8" && d[i] !== "9" && d[i] !== "A" && d[i] !== "B" && d[i] !== "C" && d[i] !== "D" && d[i] !== "E" && d[i] !== "F"&& d[i] !== "a"&& d[i] !== "b"&& d[i] !== "c"&& d[i] !== "d"&& d[i] !== "e"&& d[i] !== "f") {
+                        if (d[i] !== "0" && d[i] !== "1" && d[i] !== "2" && d[i] !== "3" && d[i] !== "4" && d[i] !== "5" && d[i] !== "6" && d[i] !== "7" && d[i] !== "8" && d[i] !== "9" && d[i] !== "A" && d[i] !== "B" && d[i] !== "C" && d[i] !== "D" && d[i] !== "E" && d[i] !== "F" && d[i] !== "a" && d[i] !== "b" && d[i] !== "c" && d[i] !== "d" && d[i] !== "e" && d[i] !== "f") {
                                 valid = false;
                                 break;
                         }
@@ -400,11 +394,11 @@ menu[6].addEventListener('click', () => {
                         inputs[0].classList.remove('red');
                 }
         });
-        inputs[1].addEventListener('input',()=>{
+        inputs[1].addEventListener('input', () => {
                 let valid = true;
-                let d=inputs[1].value;
+                let d = inputs[1].value;
                 for (let i = 0; i < d.length; i++) {
-                        if (d[i] !== "0" && d[i] !== "1" && d[i] !== "2" && d[i] !== "3" && d[i] !== "4" && d[i] !== "5" && d[i] !== "6" && d[i] !== "7" && d[i] !== "8" && d[i] !== "9" && d[i] !== "A" && d[i] !== "B" && d[i] !== "C" && d[i] !== "D" && d[i] !== "E" && d[i] !== "F"&& d[i] !== "a"&& d[i] !== "b"&& d[i] !== "c"&& d[i] !== "d"&& d[i] !== "e"&& d[i] !== "f") {
+                        if (d[i] !== "0" && d[i] !== "1" && d[i] !== "2" && d[i] !== "3" && d[i] !== "4" && d[i] !== "5" && d[i] !== "6" && d[i] !== "7" && d[i] !== "8" && d[i] !== "9" && d[i] !== "A" && d[i] !== "B" && d[i] !== "C" && d[i] !== "D" && d[i] !== "E" && d[i] !== "F" && d[i] !== "a" && d[i] !== "b" && d[i] !== "c" && d[i] !== "d" && d[i] !== "e" && d[i] !== "f") {
                                 valid = false;
                                 break;
                         }
